f9a090984562b4d1dc81b59c48cc825099acf17b
DatsOlena <datsolena@gmail.com>
2026-08-05 11:45:44 +0200

    Add ui, implement fetch todos on initial mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import Footer from './features/core/components/Footer'
 import Header from './features/core/components/Header'
 import TodoList from './features/todo/components/TodoList'
+import { fetchTodos } from './features/todo/services/todo.service.ts'
+import { useEffect, useState } from 'react'
+import type { Todo } from './features/todo/types/todo.type'
+import Filters from './features/todo/components/TodoFilters'
+import CreateTodoForm from './features/todo/components/CreateTodoForm'
+import Button from './features/shared/components/Button'
+import Modal from './features/shared/components/Modal'
 
 export default function App() {
+const [todos, setTodos] = useState<Todo[]>([])
+const [isModalOpen, setIsModalOpen] = useState(false)
+
+  useEffect(() => {
+    const fetchedTodos = async () => {
+      const todos = await fetchTodos()
+      setTodos(todos)
+      console.log(todos)
+    }
+    fetchedTodos()
+  }, [])
+
   return (
     <div className='flex flex-col min-h-screen text-left'>
       <Header />
       <main className='flex-grow px-4 py-8'>
-        <div className='max-w-3xl mx-auto'>
-          <h1 className='text-center'>Your todos</h1>
-          <TodoList todos={[]} />
-        </div>
+        <Filters filter={{ status: 'all', priority: 'all' }} onChange={() => {}} />
+        <TodoList todos={todos} />
+        <Button text='Add Todo' onClick={() => { setIsModalOpen(true) }} />
+        {isModalOpen && ( 
+        <Modal title='Add Todo' onClose={() => { setIsModalOpen(false) }}>
+          <CreateTodoForm onCreate={() => {  }} onCancel={() => { setIsModalOpen(false) }} />
+        </Modal>
+        )}
       </main>
       <Footer />
     </div>
